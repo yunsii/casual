e13fa34af33103db9a5dcf0a542e80f42da83a27
theprimone <977501258@qq.com>
2018-08-27 19:35:59 +0800

    主要更新
    添加方法：调用有返回值的存储过程

--- theprimone.py
+++ theprimone.py
@@ -1,11 +1,117 @@
 import re, time, requests
-import pymysql
-from json import dump, loads
+import pymysql, datetime
+from json import dump, loads, dumps
 from DBUtils.PooledDB import PooledDB
+from configparser import ConfigParser
 
 
 def sleep_(st):
     time.sleep(st)
+
+
+# ------------------------------------------------------------
+#                       对象相关
+# ------------------------------------------------------------
+
+
+def merge_dict(dict_, dict_another):
+    """
+    字典对象合并
+    :param dict_:
+    :param dict_another:
+    :return:
+    """
+    return dict(dict_, **dict_another)
+
+
+# ------------------------------------------------------------
+#                       时间转换相关
+# ------------------------------------------------------------
+
+
+def get_int_time_now():
+    """
+    系统当前时间戳，以秒计
+    :return:
+    """
+    return int(time.time())
+
+
+def get_format_datetime_now():
+    """
+    格式化时间戳strftime && strptime
+    :return:
+    """
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+
+
+def str_time2int(str_time: str):
+    """
+    转换时间字符串为时间戳
+    :param str_time:
+    :return:
+    """
+    stamp = int()
+    try:
+        datetime_list = re.split(r"[- :]", str_time)
+        datetime_list = [i for i in datetime_list if i]
+        print(datetime_list)
+        if len(datetime_list) == 3:
+            stamp = int(time.mktime(time.strptime(str_time, "%Y-%m-%d")))
+        elif len(datetime_list) == 5:
+            stamp = int(time.mktime(time.strptime(str_time, "%Y-%m-%d %H:%M")))
+        elif len(datetime_list) == 6:
+            stamp = int(time.mktime(time.strptime(str_time, "%Y-%m-%d %H:%M:%S")))
+        else:
+            stamp = str_time
+    except Exception as e:
+        print("Exception str_time:", str_time)
+        print(e)
+    finally:
+        return stamp
+
+
+def int2str_time(stamp, tplt="normal"):
+    """
+    时间戳转格式化时间字符串
+    :param stamp:
+    :param tplt:
+    :return:
+    """
+    if tplt == "short":
+        tplt = "%Y-%m-%d"
+    elif tplt == "normal":
+        tplt = "%Y-%m-%d %H:%M:%S"
+    elif tplt == "withoutSec":
+        tplt = "%Y-%m-%d %H:%M"
+    return datetime.datetime.fromtimestamp(stamp).strftime(tplt)
+
+# ------------------------------------------------------------
+#                     json conf 等文件相关
+# ------------------------------------------------------------
+
+
+def get_value_from_conf(file_path, section, option):
+    """
+    获取配置文件唯一值
+    :param file_path:
+    :param section:
+    :param option:
+    :return:
+    """
+    conf = ConfigParser()  # 实例化
+    conf.read(file_path, "utf-8")
+    return conf.get(section, option)
+
+
+def dict2format_string(dict_, indent=4):
+    """
+    字典格式化输出字符串
+    :param dict_:
+    :param indent:
+    :return:
+    """
+    return dumps(dict_, ensure_ascii=False, indent=indent)
 
 
 def dict2json(dict_, path_):
@@ -30,23 +136,6 @@ def json2dict(path_):
     return dict_
 
 
-def str_time2int(str_time):
-    """
-    转换时间字符串为时间戳
-    :param str_time:
-    :return:
-    """
-    datetime_list = re.split(r"[- :]", str_time)
-    datetime_list = [i for i in datetime_list if i]
-    # print(datetime_list)
-    if len(datetime_list) == 3:
-        return int(time.mktime(time.strptime(str_time, "%Y-%m-%d")))
-    elif len(datetime_list) == 5:
-        return int(time.mktime(time.strptime(str_time, "%Y-%m-%d %H:%M")))
-    elif len(datetime_list) == 6:
-        return int(time.mktime(time.strptime(str_time, "%Y-%m-%d %H:%M:%S")))
-
-
 def cookie_str2json(str_):
     """
     请求头中cookie值保存到本地json文件
@@ -63,22 +152,26 @@ def cookie_str2json(str_):
     dict2json(cookies_dict, "./json/cookies.json")
 
 
-def get_response(url, method="GET", headers=None, data=None, encoding_=None, **kwargs):
+def get_response(url, method="GET", is_api=False, headers=None, params=None, data=None, encoding_=None, **kwargs):
     """
     网页请求
     :param url:
     :param method:
+    :param is_api:
     :param headers:
-    :param data:
+    :param params:  请求参数
+    :param data:  表单数据
     :param encoding_:
     :return:
     """
     try:
-        res = requests.request(method=method, url=url, headers=headers, data=data, timeout=30, **kwargs)
+        res = requests.request(method=method, url=url, headers=headers, params=params, data=data, timeout=30, **kwargs)
         if encoding_:
             res.encoding = encoding_
         else:
             res.encoding = res.apparent_encoding
+        if is_api:
+            return res.json()
         return res
     except Exception as e:
         print("Error", e)
@@ -193,6 +286,23 @@ def dict2update_sql(dict_, table, where_condition):
     return update_sql
 
 
+def dict2insert_sql(table, dict_list):
+    """
+    字段转换为insert语句
+    :param table:
+    :param dict_list:
+    :return:
+    """
+    if isinstance(dict_list, dict):
+        dict_list = [dict_list]
+
+    insert_tplt = "INSERT INTO {}({}) VALUES{}"
+    keys = list(dict_list[0].keys())
+    values = [list(map(str, list(x.values()))) for x in dict_list]
+    values = ["{0}{1}{2}".format("('", "', '".join(x), "')") for x in values]
+    return insert_tplt.format(table, ", ".join(keys), ", ".join(values))
+
+
 class MysqlUtil(object):
     __pool = None
 
@@ -219,7 +329,11 @@ class MysqlUtil(object):
 
     def update_(self, sql):
         """
-        执行一条更新语句 插入\更新\删除\建表
+        执行一条更新语句，包括无返回值存储过程
+        插入\ 1 插入 2 重写
+        更新\
+        删除\
+        建表
         :param sql:
         :return:
         """
@@ -243,8 +357,11 @@ class MysqlUtil(object):
             sql_list = [sql_list]
         for sql in sql_list:
             # print(sql)
-            insert_num = self.cur.execute(sql)
-            insert_count += insert_num
+            try:
+                insert_num = self.cur.execute(sql)
+                insert_count += insert_num
+            except Exception as e:
+                print("Error: {}\nSQL: {}".format(e, sql))
         self.conn.commit()
         return insert_count
 
@@ -261,17 +378,33 @@ class MysqlUtil(object):
             results = list()
         return results
 
+    def call_proc(self, proc_name, out_count=0, **args):
+        """
+        执行存储过程
+            无返回值存储过程可通过 update_() 直接调用
+        :param proc_name:
+        :param out_count:
+        :param args:
+        :return:
+        """
+        args = list(args.values())
+        self.cur.callproc(proc_name, args)
+        if out_count:
+            out_args = ["@_{}_{} {}".format(proc_name, x, args[x]) for x in range(len(args) - out_count, len(args))]
+            return self.select_("SELECT {}".format(", ".join(out_args)))
+        return list()
+
     def close(self):
         """
         释放资源
         :return:
         """
-        self.conn.close()
         self.cur.close()
+        self.conn.close()
 
 
 if __name__ == "__main__":
-    # print(json2dict("./json/cookies.json"))
-    # escape_db_string("a\'b")
-    print(str_time2int("2018-06-10  23:37:30"))
+    # print((datetime.datetime(2018, 8, 26, 19, 15, 8)).timestamp())
+    print(dict2insert_sql("table", {"link": "977", "room_id": 777}))
     pass
+
